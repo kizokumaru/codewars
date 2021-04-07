57cf93c3c10fe1baa360e3495e522f00806d802e
Nestor López Royo <nestorlopezroyo@gmail.com>
2021-04-07 21:18:15 +0200

    PaginationHelper

---
+++ codewarsjava/src/test/java/com/home/codewars/PaginationHelperTest.java
@@ -0,0 +1,16 @@
+package com.home.codewars;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class PaginationHelperTest {
+
+    // TODO: Replace examples and use TDD by writing your own tests
+
+    @Test
+    public void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+
+}
